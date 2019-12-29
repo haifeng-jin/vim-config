a4bd6851388a284ea8deede58f4f4e0e5ec93f05
Haifeng Jin <jhfjhfj1@gmail.com>
2019-12-28 23:12:48 -0600

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# python-vim-ide
-My configuration for my vim as a python ide
+# VIM Configuration
+My configuration for VIM.
--- config.sh
+++
@@ -1,5 +0,0 @@
-./configure \
-  --enable-pythoninterp=yes \
-  --enable-python3interp=yes \
-  --with-python-command=python \
-  --with-python3-command=python3.6
